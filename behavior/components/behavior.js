68406b04c9edcf971b0ae3d2b570aba8a301d90c
wyunfei <583514368@qq.com>
2018-12-06 12:44:35 +0800

    behavior-demo

---
+++ behavior/components/behavior.js
@@ -0,0 +1,8 @@
+// 创建behavior.js供movie、music组件使用
+let SmallFourBeh = Behavior({
+  properties: {
+    name: String,
+    type: String
+  }
+})
+export { SmallFourBeh } // 导出
---
+++ behavior/components/movie/index.js
@@ -0,0 +1,10 @@
+// components/movie/index.js
+import { SmallFourBeh } from '../behavior.js' // 导入behavior.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  behaviors: [SmallFourBeh], //  继承behavior.js里面的properties
+  properties: {
+  }
+})
